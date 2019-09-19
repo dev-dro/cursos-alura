d9cdd9f2e38e2e05cf7e6eb4aa457a4a538854c7
dev-dro <danilo.ro@protonmail.com>
2019-09-19 20:10:19 -0300

    backup

--- javascript/typescript/alurabank/app/js/controllers/NegociacaoController.js
+++ javascript/typescript/alurabank/app/js/controllers/NegociacaoController.js
@@ -1,12 +1,21 @@
 class NegociacaoController {
+    private _inputData: HttpInputElement;
+    private _inputQuantidade: HttpInputElement;
+    private _inputValor: HttpInputElement;
+    private _negociacoes: Negociacoes = new Negociacoes();
+
     constructor() {
         this._inputData = document.querySelector('#data');
         this._inputQuantidade = document.querySelector('#quantidade');
         this._inputValor = document.querySelector('#valor');
+        this._negociacoes = 
     }
-    adiciona(event) {
+    adiciona(event: Event) {
         event.preventDefault();
-        const negociacao = new Negociacao(new Date(this._inputData.value.replace(/-/g, '/')), parseInt(this._inputQuantidade.value), parseInt(this._inputValor.value));
+        const negociacao = new Negociacao(
+            new Date(this._inputData.value.replace(/-/g, '/')), 
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value));
         console.log(negociacao);
     }
 }
